d8235b1a41c48439c8887c9aed144caf602ca031
rayphee <r.mueen@gmail.com>
2016-01-12 01:16:44 -0500

    Layout Page 1 Complete, All Audio Working
    Page 1 Layout is complete, minor edits still on the way to make full
    use of iPhone 6, 6 plus screen sizes; all audio units hooked up. Very
    close to being done.

--- iOS/DJ Khaled Soundboard/DJ Khaled Soundboard/View3.swift
+++ iOS/DJ Khaled Soundboard/DJ Khaled Soundboard/View3.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class View3: UIViewController {
 
@@ -21,7 +22,84 @@ class View3: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    var KnowThatPlayer:[AVAudioPlayer]=[AVAudioPlayer(),AVAudioPlayer(),
+        AVAudioPlayer(),AVAudioPlayer(),AVAudioPlayer(),AVAudioPlayer(),
+        AVAudioPlayer(),AVAudioPlayer(),AVAudioPlayer(),AVAudioPlayer()]
+    var ktp = 0
+    @IBAction func KnowThat(sender: AnyObject) {
+        let path = NSBundle.mainBundle().pathForResource("knowThat.mp3", ofType:nil)!
+        let url = NSURL(fileURLWithPath: path)
+        //let AnotherOneUrl = NSURL(fileURLWithPath: "/Users/rafi/Dropbox/Business Docs/Panyk Tech/App Devil/TheyDontWantUsToMakeAnApp/iOS/DJ Khaled Soundboard/DJ Khaled Soundboard/AnotherOne.mp3")
+        do{
+            try KnowThatPlayer[ktp%10] = AVAudioPlayer(contentsOfURL: url)
+            KnowThatPlayer[ktp%10].prepareToPlay()
+            KnowThatPlayer[ktp%10].play();
+            ktp++;
+            //NSLog("Button working");
+        }catch{
+            //do nothing
+        }
+    }
 
+    var BowDownPlayer:[AVAudioPlayer]=[AVAudioPlayer(),AVAudioPlayer(),
+        AVAudioPlayer(),AVAudioPlayer(),AVAudioPlayer(),AVAudioPlayer(),
+        AVAudioPlayer(),AVAudioPlayer(),AVAudioPlayer(),AVAudioPlayer()]
+    var bdp = 0
+    @IBAction func BowDown(sender: AnyObject) {
+        let path = NSBundle.mainBundle().pathForResource("bowDown.mp3", ofType:nil)!
+        let url = NSURL(fileURLWithPath: path)
+        //let AnotherOneUrl = NSURL(fileURLWithPath: "/Users/rafi/Dropbox/Business Docs/Panyk Tech/App Devil/TheyDontWantUsToMakeAnApp/iOS/DJ Khaled Soundboard/DJ Khaled Soundboard/AnotherOne.mp3")
+        do{
+            try BowDownPlayer[bdp%10] = AVAudioPlayer(contentsOfURL: url)
+            BowDownPlayer[bdp%10].prepareToPlay()
+            BowDownPlayer[bdp%10].play();
+            bdp++;
+            //NSLog("Button working");
+        }catch{
+            //do nothing
+        }
+    }
+    
+    var AnotherCrabPlayer:[AVAudioPlayer]=[AVAudioPlayer(),AVAudioPlayer(),
+        AVAudioPlayer(),AVAudioPlayer(),AVAudioPlayer(),AVAudioPlayer(),
+        AVAudioPlayer(),AVAudioPlayer(),AVAudioPlayer(),AVAudioPlayer()]
+    var acp = 0
+    @IBAction func AnotherCrab(sender: AnyObject) {
+        let path = NSBundle.mainBundle().pathForResource("anotherCrab.mp3", ofType:nil)!
+        let url = NSURL(fileURLWithPath: path)
+        //let AnotherOneUrl = NSURL(fileURLWithPath: "/Users/rafi/Dropbox/Business Docs/Panyk Tech/App Devil/TheyDontWantUsToMakeAnApp/iOS/DJ Khaled Soundboard/DJ Khaled Soundboard/AnotherOne.mp3")
+        do{
+            try AnotherCrabPlayer[acp%10] = AVAudioPlayer(contentsOfURL: url)
+            AnotherCrabPlayer[acp%10].prepareToPlay()
+            AnotherCrabPlayer[acp%10].play();
+            acp++;
+            //NSLog("Button working");
+        }catch{
+            //do nothing
+        }
+    }
+    
+    var BambooPlayer:[AVAudioPlayer]=[AVAudioPlayer(),AVAudioPlayer(),
+        AVAudioPlayer(),AVAudioPlayer(),AVAudioPlayer(),AVAudioPlayer(),
+        AVAudioPlayer(),AVAudioPlayer(),AVAudioPlayer(),AVAudioPlayer()]
+    var bpl = 0
+    @IBAction func Bamboo(sender: AnyObject) {
+        let path = NSBundle.mainBundle().pathForResource("Bamboo.mp3", ofType:nil)!
+        let url = NSURL(fileURLWithPath: path)
+        //let AnotherOneUrl = NSURL(fileURLWithPath: "/Users/rafi/Dropbox/Business Docs/Panyk Tech/App Devil/TheyDontWantUsToMakeAnApp/iOS/DJ Khaled Soundboard/DJ Khaled Soundboard/AnotherOne.mp3")
+        do{
+            try BambooPlayer[bpl%10] = AVAudioPlayer(contentsOfURL: url)
+            BambooPlayer[bpl%10].prepareToPlay()
+            BambooPlayer[bpl%10].play();
+            bpl++;
+            //NSLog("Button working");
+        }catch{
+            //do nothing
+        }
+    }
+    
+    
+    
     /*
     // MARK: - Navigation
 
